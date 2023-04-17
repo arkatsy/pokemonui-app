a67f6a9e2785fe47ae1a3f8827f877e1ad2a4086
Argyris <arkatsy@gmail.com>
2023-04-17 15:30:14 +0300

    implement infinite scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
 import styles from "./styles/home.module.css";
 import { ThemeSwitch } from "./components";
 import { usePreviewPokemons } from "./hooks/usePreviewPokemons";
+import { useInView } from "react-intersection-observer";
+import { Fragment, useEffect, useState } from "react";
 
 function App() {
-  const { data, isLoading, isError } = usePreviewPokemons();
+  const {
+    status,
+    data,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    isFetchingPreviousPage,
+    fetchNextPage,
+    fetchPreviousPage,
+    hasNextPage,
+    hasPreviousPage,
+  } = usePreviewPokemons();
+  const { ref, inView } = useInView();
+  const [scrollToTopBtn, setScrollToTopBtn] = useState(false);
+
+  const handleScroll = () => {
+    if (+window.scrollY.toFixed(0) > 400) {
+      setScrollToTopBtn(true);
+    } else {
+      setScrollToTopBtn(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
   return (
     <div className={styles.page}>
@@ -16,21 +57,38 @@ function App() {
       <div className={styles.mainContainer}>
         <main className={styles.mainContent}>
           {data &&
-            data.map((pokemon) => (
-              <div className={styles.pokemonCard} key={pokemon.id}>
-                <h1 className={styles.pokemonTitle}>{pokemon.name}</h1>
-                <div>
-                  <img
-                    draggable={false}
-                    className={styles.pokemonImage}
-                    src={pokemon.image}
-                    alt={`${pokemon.name} image`}
-                  />
-                </div>
-              </div>
+            data.pages.map((page, idx) => (
+              <Fragment key={idx}>
+                {page.data.map((pokemon) => (
+                  <div className={styles.pokemonCard} key={pokemon.id}>
+                    <h1 className={styles.pokemonTitle}>{pokemon.name}</h1>
+                    <img
+                      draggable={false}
+                      className={styles.pokemonImage}
+                      src={pokemon.image}
+                      alt={`${pokemon.name} image`}
+                    />
+                  </div>
+                ))}
+              </Fragment>
             ))}
+
+          {hasNextPage && (
+            <div ref={ref} className={styles.loading}>
+              {isFetchingNextPage ? "Loading more..." : "Load More"}
+            </div>
+          )}
         </main>
       </div>
+      {scrollToTopBtn && (
+        <button
+          onClick={scrollToTop}
+          className={styles.scrollToTopBtn}
+          aria-label="Scroll to top"
+        >
+          <ScrollUpIcon />
+        </button>
+      )}
     </div>
   );
 }
@@ -42,6 +100,25 @@ function Brand() {
         <span>Poké</span>mons
       </h1>
     </a>
+  );
+}
+
+function ScrollUpIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+      />
+    </svg>
   );
 }
 
--- src/hooks/usePreviewPokemons.ts
+++ src/hooks/usePreviewPokemons.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 
 const API_URL = "https://pokeapi.co/api/v2/pokemon";
 
@@ -7,23 +7,43 @@ const DATA_SLICE = 20;
 // Gets the pokemon id from the url
 const getPokemonId = (url: string) => {
   const urlParts = url.split("/");
-  return urlParts[urlParts.length - 2];
+  return Number(urlParts[urlParts.length - 2]);
 };
 
-type PreviewPokemons = {
-  id: string;
+type PreviewPokemon = {
+  id: number;
   name: string;
   image: string;
-}[];
+};
+
+type PreviewPokemonList = {
+  data: PreviewPokemon[];
+  nextPage: number;
+  previousPage: number;
+};
 
 // Fetches all the necessary data for the preview pokemons in the home page
-const fetchPreviewPokemons = async () => {
+const fetchPreviewPokemons = async ({ pageParam }: { pageParam: number }) => {
   // `limit` limits the result to n pokemons and `offset` skips the first n pokemons
-  const response = await fetch(`${API_URL}?limit=${DATA_SLICE}&offset=0`);
+
+  // page 0 -> l = 20, o = 0
+  // page 1 -> l = 20, o = 20
+  // page 2 -> l = 20, o = 40
+  // page 3 -> l = 20, o = 60
+  // page 4 -> l = 20, o = 80
+  // ...
+  // page n -> l = 20, o = n * 20
+  const response = await fetch(
+    `${API_URL}?limit=${DATA_SLICE}&offset=${pageParam * DATA_SLICE}`
+  );
   const data = await response.json();
 
   // Fetches the data for each pokemon
-  const previewPokemons: PreviewPokemons = [];
+  const previewPokemonList: PreviewPokemonList = {
+    data: [],
+    nextPage: pageParam + 1,
+    previousPage: pageParam - 1,
+  };
   for (const pokemon of data.results) {
     const pokemonName = pokemon.name;
     const pokemonUrl = pokemon.url;
@@ -36,20 +56,25 @@ const fetchPreviewPokemons = async () => {
     const pokemonImage =
       pokemonDetailsData.sprites.other.dream_world.front_default;
 
-    previewPokemons.push({
+    previewPokemonList.data.push({
       id: pokemonId,
       name: pokemonName,
       image: pokemonImage,
     });
   }
 
-  return previewPokemons;
+  if (previewPokemonList.data.length < DATA_SLICE)
+    previewPokemonList.nextPage = -1;
+
+  return previewPokemonList;
 };
 
 export const usePreviewPokemons = () => {
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: ["pokemons"],
-    queryFn: fetchPreviewPokemons,
+    queryFn: ({ pageParam = 0 }) => fetchPreviewPokemons({ pageParam }),
+    getNextPageParam: (lastPage, pages) => lastPage.nextPage,
+    getPreviousPageParam: (firstPage, pages) => firstPage.previousPage,
     staleTime: Infinity,
   });
 };
